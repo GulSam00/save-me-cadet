c2536038dcf66f31fbefd5c7c74c41fe79c96df0
GulSam00 <gulsamcono@gmail.com>
2024-02-26 04:32:19 +0900

    Refactor : 환경 변수

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore/lite';
 
 // 나중에 환경 변수 처리
 const firebaseConfig = {
-  apiKey: 'AIzaSyCCgtyHymt42HVBdVaBEWDf8LgXBlC01yg',
-  authDomain: 'lms-project-9dc3e.firebaseapp.com',
-  projectId: 'lms-project-9dc3e',
-  storageBucket: 'lms-project-9dc3e.appspot.com',
-  messagingSenderId: '910207178093',
-  appId: '1:910207178093:web:5d9ac15070de4790937664',
-  measurementId: 'G-5RR091E8S5',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUERMENT_ID,
 };
 
 // Initialize Firebase
